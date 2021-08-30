b8283b2ae9b517844d92ab6df876995ab8f2018f
Momal Afzal <72850994+moomaal@users.noreply.github.com>
2021-08-30 14:38:14 +0500

    index page updated blog-post

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,7 +6,160 @@ import HeadData from "../components/HeadData.js";
 import { Calendar } from "../components/SVG.js";
 import SiteMetaData from "../components/SiteMetadata.js";
 import { FindCategory, FillSpace, LinkFix } from "../components/SimpleFunctions.js";
+export const BlogPostTemplate = (props) => {
+  const { frontmatter, link, tocdata, body } = props;
+  const { title: siteName, ads, disqus } = SiteMetaData();
+  const adCodes = ads?.adCodes;
+  const [btT, setBtT] = useState("");
+  const contentRef = useRef(null);
+  const [topOffset, setTopOffset] = useState(900000000);
+  const { base: img, name: imgName, childImageSharp } = frontmatter.featuredimage;
+  const { width, height } = childImageSharp.original;
+  const disqusConfig = {
+    url: link,
+    title: frontmatter.title,
+  };
+  const showAds = ads?.enableAds && !ads?.disabledPostsAds?.includes(frontmatter.slug);
 
+  const scrollTop = () => {
+    typeof window !== "undefined" &&
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+  };
+
+  const backToTop = () => {
+    window.scrollY > 300 ? setBtT("active") : setBtT("");
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", backToTop);
+
+      setTopOffset(contentRef.current.offsetTop + contentRef.current.offsetHeight - 1000);
+
+      return () => window.removeEventListener("scroll", backToTop);
+    }
+  }, [frontmatter.tableofcontent]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const ea = document.querySelectorAll("a[href^='#']");
+      const r = (e) => {
+        e.preventDefault();
+        const o = e.target.getAttribute("href").substring(1);
+        if (document.getElementById(o)) {
+          const e = window.scrollY + document.getElementById(o).getBoundingClientRect().top - 20;
+          window.scrollTo({ top: e, behavior: "smooth" });
+        }
+      };
+      for (const t of ea) {
+        t.addEventListener("click", r);
+      }
+      return () => {
+        for (const t of ea) {
+          t.removeEventListener("click", r);
+        }
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <section className="section blog-post">
+        <button onClick={() => scrollTop()} className={`btp ${btT}`}>
+          <TopArrow />
+        </button>
+        <div className="container content">
+          <div className="blog-columns">
+            <div className="column is-9">
+              <div className="blog-section-top entry-content">
+                {!frontmatter.hidefeaturedimage && (
+                  <picture className="blog-top-img">
+                    <source media="(min-width:768px)" srcSet={`/image/post-first/${imgName}.webp`} />
+                    <source media="(min-width:100px)" srcSet={`/image/post-first/${imgName}-m.webp`} />
+                    <img src={`/img/${img}`} alt={frontmatter.title} loading="lazy" width={width} height={height} />
+                  </picture>
+                )}
+                <div className="blog-section-top-inner">
+                  <h1 className="title entry-title">{frontmatter.title}</h1>
+                  <BlogInfo date={frontmatter.date} disqusConfig={disqusConfig} disqus={disqus} title={frontmatter.title} image={img} />
+                  <MDXProvider components={PostComps}>
+                    <MDXRenderer>{frontmatter.beforebody}</MDXRenderer>
+                  </MDXProvider>
+                </div>
+              </div>
+              {frontmatter.tableofcontent && !!tocdata.length && <PostComps.TableOfContents data={tocdata} />}
+              {showAds && <div className="ads-toc" dangerouslySetInnerHTML={{ __html: adCodes?.afterToC }} />}
+              {frontmatter.table?.table && frontmatter.table?.title && !!frontmatter.products?.length && <PostComps.PTitle title={frontmatter.table?.title} cName="is-bold is-center" />}
+              {frontmatter.table?.table && !!frontmatter.products?.length && <PostComps.ProductsTable products={frontmatter.products} headTitle={frontmatter.table?.headTitle} productColumns={frontmatter.table.productColumns} title={frontmatter.table?.seoTitle} />}
+              <div ref={contentRef} className="post-content">
+                <MDXProvider components={PostComps}>
+                  <MDXRenderer>{body}</MDXRenderer>
+                </MDXProvider>
+                {!!frontmatter.products?.length && (
+                  <div className="products">
+                    {frontmatter.products?.map((item, index) => (
+                      <div className="product-box" key={index}>
+                        <PostComps.PTitle hlevel="3" title={item.name} />
+                        {item.image && <PostComps.PImage alt={item.name} src={item.image.base} link={item.link} />}
+                        {!!item.specs?.length && <PostComps.SpecTable spec={item.specs} />}
+                        <MDXProvider components={PostComps}>
+                          <MDXRenderer>{item.body}</MDXRenderer>
+                        </MDXProvider>
+                        {(!!item.pros?.length || !!item.cons?.length) && <PostComps.ProsNCons pros={item.pros} cons={item.cons} />}
+                        <PostComps.BButton link={item.link} title={item.btnText} />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <div className="blog-section-bottom">
+                <MDXProvider components={PostComps}>
+                  <MDXRenderer>{frontmatter.afterbody}</MDXRenderer>
+                </MDXProvider>
+              </div>
+              {!!frontmatter.faq?.length && (
+                <div className="post-faq">
+                  <h2 className="faq-title">Frequently Asked Questions</h2>
+                  {frontmatter.faq.map((item, index) => (
+                    <div className="faq-question" key={index}>
+                      <h3 className="faq-ques">{item.ques}</h3>
+                      <p className="faq-ans">{item.ans}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {frontmatter.rating && <RatingBox count={frontmatter.rcount} value={frontmatter.rvalue} />}
+              {showAds && <div className="ads-author" dangerouslySetInnerHTML={{ __html: adCodes?.beforeAuthor }} />}
+              <Author authorID={frontmatter.author} />
+              {disqus && (
+                <div id="disqus_thread">
+                  <LazyLoad offsetTop={topOffset}>
+                    <DiscussionEmbed config={disqusConfig} shortname={disqus} />
+                  </LazyLoad>
+                </div>
+              )}
+            </div>
+            <div className="column is-3">
+              <div className="sidebar" id="sidebar" itemType="https://schema.org/WPSideBar" itemScope="itemscope">
+                <div className="site-info">
+                  <p>{siteName} is reader-supported. When you buy through links on our site, we may earn an affiliate commission.</p>
+                </div>
+                <Search />
+                <SidebarLatestPosts />
+                <SidebarTableofContents data={frontmatter.sidebar} ad={showAds && <div className="ads-sidebar" dangerouslySetInnerHTML={{ __html: adCodes?.sidebarSticky }} />} />
+              </div>
+            </div>
+          </div>
+          <LatestPosts />
+        </div>
+      </section>
+      {showAds && <div className="ads-mobile" dangerouslySetInnerHTML={{ __html: adCodes?.stickyMobile }} />}
+    </>
+  );
+};
 const IndexTemplate = ({ data }) => {
   const singlePost = data.FP.nodes[0];
   const otherPosts = data.OP.nodes;
